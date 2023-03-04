a5d0fe72b523f38c88926e347474a78f283eabe0
Steve <stevele99@yahoo.fr>
2023-03-04 19:35:05 +0100

    Projets dynamiques

--- Backend/app.js
+++ Backend/app.js
@@ -1,3 +1,61 @@
+const gallery=document.querySelector('.gallery')
+
+/*
+image.innerText="image";
+figure.appendChild(image)
+const figcaption=document.createElement("figcaption");
+figcaption.innerText='texte';
+figure.appendChild(figcaption);*/
+
+
+const createFigure = (e)=>{
+  const figure=  document.createElement("figure");
+  e.appendChild(figure);
+}
+
+const createImage= (arrayElement)=>{
+  const image= document.createElement("image");
+  image.setAttribute=arrayElement.imageUrl;
+}
+
+
+
+
+const createAllWorks = (array, id) =>{
+  for (let index = 0; index < array.length; index++) {
+    const figure = createFigure(id);
+    createImage(figure,array[index]);
+  }
+}
+
+
+
+
+const projects= async()=>{
+  let data = await fetch('http://localhost:5678/api/works');
+  console.log("data : "+data);
+  let response = await data.json();
+  for (let index = 0; index < response.length; index++) {
+      console.log("Titre du projet : "+response[index].title)
+      createFigure(gallery);
+      const child =gallery.children[index];
+      const image = document.createElement('img')
+      image.setAttribute("src",response[index].imageUrl);
+      child.appendChild(image);
+      const title= document.createElement('figcaption');
+      title.innerText=response[index].title;
+      child.appendChild(title);
+    }
+}
+
+projects();
+
+
+
+
+
+
+
 const express = require('express');
 const path = require('path');
 const cors = require('cors')
@@ -19,9 +77,15 @@ const db = require("./models");
 const userRoutes = require('./routes/user.routes');
 const categoriesRoutes = require('./routes/categories.routes');
 const worksRoutes = require('./routes/works.routes');
+const { create } = require('domain');
 db.sequelize.sync().then(()=> console.log('db is ready'));
 app.use('/api/users', userRoutes);
 app.use('/api/categories', categoriesRoutes);
 app.use('/api/works', worksRoutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 module.exports = app;
+
+
+
+
+
